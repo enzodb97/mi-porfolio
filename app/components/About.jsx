c4f8802fc7172091897b7b28a3718666536714dc
Enzo Daniel Bertolusso <151404661+enzodb97@users.noreply.github.com>
2025-01-30 00:52:07 -0300

    add motion
    add motion

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,53 +1,92 @@
 import { assets, infoList, toolsData } from "../assets/assets";
 import Image from "next/image";
-
+import { motion } from "motion/react"
 const About = ({isDarkMode}) => {
   return (
-    <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-Lato">Introducción</h4>
-      <h2 className="text-center text-5xl font-Montserrat">Sobre mí</h2>
-      <div className="flex flex-col lg:flex-row items-center">
-        <div className="w-64 sm:w-80 rounded-3xl max-w-none">
+    <motion.div id="about" className="w-full px-[12%] py-10 scroll-mt-20"
+    initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:1}}
+
+    >
+      <motion.h4 
+      initial={{opacity:0, y:-20}}
+      whileInView={{opacity:1, y:0}}
+      transition={{duration:0.5, delay:0.3}}
+      className="text-center mb-2 text-lg font-Lato">Introducción</motion.h4>
+      <motion.h2 
+      initial={{opacity:0, y:-20}}
+      whileInView={{opacity:1, y:0}}
+      transition={{duration:0.5, delay:0.5}}
+      className="text-center text-5xl font-Montserrat">Sobre mí</motion.h2>
+
+      <motion.div
+      initial={{opacity:0}}
+      whileInView={{opacity:1, y:0}}
+      transition={{duration:0.5}}
+      className="flex flex-col lg:flex-row items-center">
+        <motion.div
+        initial={{opacity:0, scale: 0.9}}
+        whileInView={{opacity:1, scale:1}}
+        transition={{duration:0.6}}
+        className="w-64 sm:w-80 rounded-3xl max-w-none">
           <Image
             src={assets.user_image}
             alt="user"
             className="w-full rounded-3xl"
           />
-        </div>
-        <div className="flex-1 mx-4">
+        </motion.div>
+        <motion.div 
+        initial={{opacity:0}}
+        whileInView={{opacity:1}}
+        transition={{duration:0.6, delay:0.8}}
+        className="flex-1 mx-4">
           <p className="mb-10 max-2-2xl font-Lato">
             Soy estudiante en Analista de Sistemas y desarrollador web fullstack
             utilizando lenguajes como React, Postgrest, NextJs, Java. Además he
             finalizado mis cursos como Test QA manual.
           </p>
-          <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
+          <motion.ul 
+          initial={{opacity:0}}
+          whileInView={{opacity:1}}
+          transition={{duration:0.8, delay:1}}
+          className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
             {infoList.map(({ icon, iconDark, title, description }, index) => (
-              <li
+              <motion.li
+              whileInView={{scale:1.05}}
                 key={index}
                 className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50"
               >
                 <Image src={isDarkMode ? iconDark : icon} alt={title} className="w-7 mt-3" />
                 <h3 className="my-4 font-semibold text-gray-700 dark:text-white">{title}</h3>
                 <p className="text-gray-600 text-sm dark:text-white">{description}</p>
-              </li>
+              </motion.li>
             ))}
-          </ul>
-          <h4 className="my-6 text-gray-700 font-Lato dark:text-white">
+          </motion.ul>
+          <motion.h4 
+          initial={{opacity:0}}
+          whileInView={{opacity:1}}
+          transition={{delay:1.5}}
+          className="my-6 text-gray-700 font-Lato dark:text-white">
             Herramientas que uso
-          </h4>
-          <ul className="flex items-center gap-3 sm:gap-5">
+          </motion.h4>
+          <motion.ul
+          whileInView={{opacity:1.1}}
+          className="flex items-center gap-3 sm:gap-5">
             {toolsData.map((tool, index)=>(
-                <li className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500" key={index}>
+                <motion.li 
+             whileHover={{scale:1.05}}
+                className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500" key={index}>
                   <Image src={tool} alt='Tool' className="w-5 sm:w-7"/>
 
-                </li>
+                </motion.li>
               ))
             }
 
-          </ul>
-        </div>
-      </div>
-    </div>
+          </motion.ul>
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 };
 
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,33 +1,51 @@
-"use client";
-
 import { assets } from "../assets/assets";
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "motion/react"
 
 const Header = () => {
   return (
     <>
       <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
-        <div>
+        <motion.div
+        initial={{scale:0}}
+        whileInView={{scale:1}}
+        transition={{duration:0.8, type:'spring', stiffness:100}}
+        >
           <Image
             src={assets.profile_img}
             alt="perfil"
             className="rounded-full w-32"
           />
-        </div>
-        <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Montserrat">
+        </motion.div>
+        <motion.h3 
+         initial={{y:-20, opacity: 0}}
+         whileInView={{y:0, opacity:1}}
+         transition={{duration:0.6, delay:0.3}}
+        className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Montserrat">
           Hola! Soy Enzo Bertolusso{" "}
           <Image src={assets.hand_icon} alt="hand" className="w-6" />
-        </h3>
-        <h1 className="text-3xl sm:text-6xl lg:text-[-66px] font-Montserrat">
+        </motion.h3>
+        <motion.h1 
+         initial={{y:-30, opacity: 0}}
+         whileInView={{y:0, opacity:1}}
+         transition={{duration:0.8, delay:0.5}}
+        className="text-3xl sm:text-6xl lg:text-[-66px] font-Montserrat">
           Desarrollador Web FullStack
-        </h1>
-        <p>
+        </motion.h1>
+        <motion.p 
+         initial={{opacity: 0}}
+         whileInView={{opacity:1}}
+         transition={{duration:0.6, delay:0.7}}
+        >
           Soy desarrollador web fullstack de Rosario, ARG con experiencia en
           lenguajes como Java y NextJs
-        </p>
+        </motion.p>
         <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
-          <Link
+          <motion.a
+          initial={{y:30, opacity: 0}}
+          whileInView={{y:0, opacity:1}}
+          transition={{duration:0.6, delay:1}}
             href="#contact"
             className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2
             dark:bg-transparent"
@@ -38,16 +56,19 @@ const Header = () => {
               alt="contact"
               className="w-4"
             />{" "}
-          </Link>
+          </motion.a>
 
-          <a
+          <motion.a
+          initial={{y:30, opacity: 0}}
+          whileInView={{y:0, opacity:1}}
+          transition={{duration:0.6, delay:1.2}}
             href="/sample-resume.pdf"
             download
             className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
           >
             Mi Curriculum{" "}
             <Image src={assets.download_icon} alt="contact" className="w-4" />{" "}
-          </a>
+          </motion.a>
         </div>
       </div>
     </>
